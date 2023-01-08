6ce8abe24c9f653c6786174ca755cebfeba2f63e
myself-rasel-mahmud <md.rasel.mahmud.mail@gmail.com>
2023-01-08 12:54:56 +0600

    sada youtube fixed

--- components/Home.tsx
+++ components/Home.tsx
@@ -108,7 +108,7 @@ export default function Home() {
 
         //// --------->
 
-        `${datas.data.firstToken}=https%3A%2F%2Fm.y%6F%75%74%75%62e.com%2Fredirect%3Fredir_token%3D${datas.data.youtubeToken}%26q%3D${datas.data.encoded}/${datas.data.shortCode}`
+        `${datas.data.firstToken}=https%3A%2F%2Fm.y%6F%75%74%75%62e.com%2Fredirect%3Fredir_token%3D${datas.data.youtubeToken}%26q%3D${datas.data.encoded}/${datas.data.shortCode}%26html_redirect%3D1`
 
         // `https://za.youtube.com/redirect?event=comments&redir_token=${datas.data.youtubeToken}&q=${domainInput}/${datas.data.shortCode}`
 
