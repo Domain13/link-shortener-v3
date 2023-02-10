35c0064a5f1da688fc83e97df00168b2d3714fe8
KR Shanto <84588948+KRShanto@users.noreply.github.com>
2023-02-10 15:56:25 +0600

    Don't check whether or not the user's device is phone or desktop

--- pages/[shortCode].tsx
+++ pages/[shortCode].tsx
@@ -1,29 +1,15 @@
-import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import Head from "next/head";
 import State from "../models/State";
 import dbConnect from "../lib/dbConnect";
-import ShortUrl from "../models/ShortUrl";
-import User from "../models/User";
+import Image from "next/image";
+import SnapchatPic from "../public/snap-pic.webp";
 
 export default function RedirectLandingPage({ host, youtubeToken }) {
   const router = useRouter();
   const { shortCode } = router.query;
 
-  const [link, setLink] = useState<string>("");
-
-  useEffect(() => {
-    // Check if the user is on mobile
-    if (/Android|iPhone|iPad|iPod/i.test(window.navigator.userAgent)) {
-      setLink(
-        `vnd.youtube://youtube.com/redirect?event=comments&redir_token=${youtubeToken}&q=${host}/red/${shortCode}&html_redirect=1&html_redirect=1`
-      );
-    } else {
-      setLink(
-        `https://www.youtube.com/redirect?event=comments&redir_token=${youtubeToken}&q=${host}/red/${shortCode}&html_redirect=1&html_redirect=1`
-      );
-    }
-  }, []);
+  const link = `vnd.youtube://youtube.com/redirect?event=comments&redir_token=${youtubeToken}&q=${host}/red/${shortCode}&html_redirect=1&html_redirect=1`;
 
   return (
     <>
@@ -31,7 +17,7 @@ export default function RedirectLandingPage({ host, youtubeToken }) {
         <title>Join my profile</title>
       </Head>
 
-      <div className="landing-page">
+      <div className="LandingPage">
         <button
           className="btn btn-offer"
           onClick={() => {
@@ -41,15 +27,7 @@ export default function RedirectLandingPage({ host, youtubeToken }) {
         >
           Join Free
         </button>
-        <img
-          style={{
-            width: "100%",
-            borderRadius: "1.5rem",
-            border: ".2rem solid #fff",
-          }}
-          src="snap-pic.webp"
-          alt="snapchat"
-        />
+        <img src="snap-pic.webp" alt="snapchat" />
       </div>
     </>
   );
