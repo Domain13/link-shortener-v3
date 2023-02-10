b087c6ac9c87e6a5ed1d9a5dd253015c1795232e
KR Shanto <84588948+KRShanto@users.noreply.github.com>
2023-02-10 16:08:11 +0600

    Remove `encoded` field from the `Domain` model

--- models/Domain.ts
+++ models/Domain.ts
@@ -10,21 +10,6 @@ const domainSchema = new Schema({
     type: String,
     required: true, // If empty, no direct to any page. Just return 404
   },
-  encoded: {
-    type: String,
-    default: "",
-  },
-});
-
-domainSchema.pre("save", function (next) {
-  // double encode the domain name using encodeURIComponent
-  const domain = this as any;
-  const domainName = domain.domain;
-  const encoded = encodeURIComponent(encodeURIComponent(domainName));
-
-  domain.encoded = encoded;
-
-  next();
 });
 
 export default mongoose.models.Domain || mongoose.model("Domain", domainSchema);
