fcf965c352e569713c387ff1468d131b1e46407f
KR Shanto <84588948+KRShanto@users.noreply.github.com>
2023-02-12 09:07:16 +0600

    FEAT: add search feature (domains and users)

--- components/dashboard/table/Domains.tsx
+++ components/dashboard/table/Domains.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { DomainContext } from "../contexts/domain";
 import { PopupContext } from "../../../contexts/popup";
 import { CurrentRedirectLinkContext } from "../contexts/currentRedirectLink";
@@ -14,6 +14,9 @@ export default function Domains() {
     UserIdForChangeRedirectLinkContext
   );
 
+  const [domainsToShow, setDomainsToShow] = useState(domains);
+  const [domainSearch, setDomainSearch] = useState("");
+
   async function afterDeleteDomain(json: ReturnedJsonType, body: any) {
     if (json.type === "SUCCESS") {
       console.log("json", json);
@@ -22,52 +25,89 @@ export default function Domains() {
     }
   }
 
+  async function handleSearchDomain(e: any) {
+    e.preventDefault();
+
+    if (domainSearch.length === 0) {
+      setDomainsToShow(domains);
+    } else {
+      setDomainsToShow(
+        domains.filter((domain) =>
+          domain.domain.toLowerCase().includes(domainSearch.toLowerCase())
+        )
+      );
+    }
+  }
+
   return (
-    <div className="data domains">
-      <h4 className="header">Domains</h4>
-      <table>
-        <thead>
-          <tr>
-            <th>Domain</th>
-            <th>Error page</th>
-            <th>Options</th>
-          </tr>
-        </thead>
-        <tbody>
-          {domains.map((domain, index) => (
-            <tr key={index} className={index % 2 === 0 ? "even" : "odd"}>
-              <td>{domain.domain}</td>
-              <td>
-                {domain.errorPage} <br />
-              </td>
-              <td>
-                <div className="options">
-                  <button
-                    className="btn green"
-                    onClick={() => {
-                      setUserIdForChangeRedirectLink(domain._id);
-                      setCurrentRedirectLink(domain.errorPage);
-                      setPopup("ChangeRedirectLink");
-                    }}
-                  >
-                    Edit
-                  </button>
-                  <PostButton
-                    path="/api/delete_domain"
-                    body={{
-                      _id: domain._id,
-                    }}
-                    afterPost={afterDeleteDomain}
-                    className="btn red"
-                  >
-                    Delete
-                  </PostButton>
-                </div>
-              </td>
+    <div className="table">
+      <form className="search form" onSubmit={handleSearchDomain}>
+        <div className="form-wrapper label-input">
+          <input
+            type="text"
+            placeholder="Search"
+            value={domainSearch}
+            onChange={(e) => setDomainSearch(e.target.value)}
+          />
+          <div
+            className="cancel"
+            onClick={() => {
+              setDomainSearch("");
+              setDomainsToShow(domains);
+            }}
+          >
+            X
+          </div>
+        </div>
+        <button className="btn green">Search</button>
+      </form>
+
+      <div className="data domains">
+        <h4 className="header">Domains</h4>
+        <table>
+          <thead>
+            <tr>
+              <th>Domain</th>
+              <th>Error page</th>
+              <th>Options</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {domainsToShow.map((domain, index) => (
+              <tr key={index} className={index % 2 === 0 ? "even" : "odd"}>
+                <td>{domain.domain}</td>
+                <td>
+                  {domain.errorPage} <br />
+                </td>
+                <td>
+                  <div className="options">
+                    <button
+                      className="btn green"
+                      onClick={() => {
+                        setUserIdForChangeRedirectLink(domain._id);
+                        setCurrentRedirectLink(domain.errorPage);
+                        setPopup("ChangeRedirectLink");
+                      }}
+                    >
+                      Edit
+                    </button>
+                    <PostButton
+                      path="/api/delete_domain"
+                      body={{
+                        _id: domain._id,
+                      }}
+                      afterPost={afterDeleteDomain}
+                      className="btn red"
+                    >
+                      Delete
+                    </PostButton>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
--- components/dashboard/table/Users.tsx
+++ components/dashboard/table/Users.tsx
@@ -1,10 +1,11 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { UsersContext } from "../contexts/users";
 import { PopupContext } from "../../../contexts/popup";
 import { UserInfoForChangeDomainContext } from "../contexts/userInfoForChangeDomain";
 import { ChangeAffiliateCodesContext } from "../contexts/changeAffiliateCode";
 import PostButton from "../../utils/PostButton";
 import { ReturnedJsonType } from "../../../types/json";
+import Form from "../../utils/Form";
 
 export default function Users() {
   const { users, setUsers } = useContext(UsersContext);
@@ -20,7 +21,8 @@ export default function Users() {
   } = useContext(ChangeAffiliateCodesContext);
   const { setPopup } = useContext(PopupContext);
 
-  // ************************** //
+  const [usersToShow, setUsersToShow] = useState(users);
+  const [usernameSearch, setUsernameSearch] = useState("");
 
   // After the user is deleted, update the state
   async function afterDeleteUser(json: ReturnedJsonType, body: any) {
@@ -62,116 +64,153 @@ export default function Users() {
     }
   }
 
+  async function handleSearchUser(e: any) {
+    e.preventDefault();
+
+    if (usernameSearch === "") {
+      setUsersToShow(users);
+    } else {
+      setUsersToShow(
+        users.filter((user) =>
+          user.username.toLowerCase().includes(usernameSearch.toLowerCase())
+        )
+      );
+    }
+  }
+
   return (
-    <div className="data users">
-      <h4 className="header">Users</h4>
-      <table>
-        <thead>
-          <tr>
-            <th>Username</th>
-            <th>Role</th>
-            <th>Domain</th>
-            <th>Code</th>
-            <th>Redirect</th>
-            <th>Always Redirect</th>
-            <th>Delete</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user, index) => (
-            <tr key={user._id} className={index % 2 === 0 ? "even" : "odd"}>
-              <td>{user.username}</td>
-              <td>{user.role}</td>
-              <td>
-                {
+    <div className="table">
+      <form className="search form" onSubmit={handleSearchUser}>
+        <div className="form-wrapper label-input">
+          <input
+            type="text"
+            placeholder="Search"
+            value={usernameSearch}
+            onChange={(e) => setUsernameSearch(e.target.value)}
+          />
+          <div
+            className="cancel"
+            onClick={() => {
+              setUsernameSearch("");
+              setUsersToShow(users);
+            }}
+          >
+            X
+          </div>
+        </div>
+        <button className="btn green">Search</button>
+      </form>
+
+      <div className="data users">
+        <h4 className="header">Users</h4>
+        <table>
+          <thead>
+            <tr>
+              <th>Username</th>
+              <th>Role</th>
+              <th>Domain</th>
+              <th>Code</th>
+              <th>Redirect</th>
+              <th>Always Redirect</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {usersToShow.map((user, index) => (
+              <tr key={user._id} className={index % 2 === 0 ? "even" : "odd"}>
+                <td>{user.username}</td>
+                <td>{user.role}</td>
+                <td>
+                  {
+                    <div className="text-with-button">
+                      <p className="text">{user.domain}</p>
+                      <button
+                        className="btn green"
+                        onClick={() => {
+                          setDomainForChange(user.domain);
+                          setUserIdForDomainChange(user._id);
+                          setUsernameForDomainChange(user.username);
+                          setPopup("ChangeDomain");
+                        }}
+                      >
+                        Change
+                      </button>
+                    </div>
+                  }
+                </td>
+                <td>
+                  {
+                    <div className="text-with-button">
+                      <p className="text">{user.affiliateCodes.join(", ")}</p>
+                      <button
+                        className="btn green"
+                        onClick={() => {
+                          setUserIdForChangeAffiliateCodes(user._id);
+                          setAffiliateCodesForChange(user.affiliateCodes);
+                          setUsernameForChangeAffiliateCodes(user.username);
+                          setPopup("ChangeAffiliateCode");
+                        }}
+                      >
+                        Change
+                      </button>
+                    </div>
+                  }
+                </td>
+                <td>
                   <div className="text-with-button">
-                    <p className="text">{user.domain}</p>
-                    <button
-                      className="btn green"
-                      onClick={() => {
-                        setDomainForChange(user.domain);
-                        setUserIdForDomainChange(user._id);
-                        setUsernameForDomainChange(user.username);
-                        setPopup("ChangeDomain");
-                      }}
-                    >
-                      Change
-                    </button>
-                  </div>
-                }
-              </td>
-              <td>
-                {
-                  <div className="text-with-button">
-                    <p className="text">{user.affiliateCodes.join(", ")}</p>
-                    <button
-                      className="btn green"
-                      onClick={() => {
-                        setUserIdForChangeAffiliateCodes(user._id);
-                        setAffiliateCodesForChange(user.affiliateCodes);
-                        setUsernameForChangeAffiliateCodes(user.username);
-                        setPopup("ChangeAffiliateCode");
-                      }}
-                    >
-                      Change
-                    </button>
-                  </div>
-                }
-              </td>
-              <td>
-                <div className="text-with-button">
-                  <p className="text">
-                    Status: <b>{user.shouldRedirectOnLimit ? "On" : "Off"}</b>
-                  </p>
-                  <PostButton
-                    path="/api/change_redirect_config"
-                    body={{
-                      _id: user._id,
-                    }}
-                    afterPost={afterChangeRedirectConfig}
-                    className="btn green"
-                  >
-                    Turn {user.shouldRedirectOnLimit ? "Off" : "On"}
-                  </PostButton>
-                </div>
-              </td>
-              <td>
-                <div className="text-with-button">
-                  <p className="text">
-                    Status: <b>{user.shouldAlwaysRedirect ? "On" : "Off"}</b>
-                  </p>
-                  <PostButton
-                    path="/api/change_always_redirect_config"
-                    body={{
-                      _id: user._id,
-                    }}
-                    afterPost={afterChangeAlwaysRedirectConfig}
-                    className="btn green"
-                  >
-                    Turn {user.shouldAlwaysRedirect ? "Off" : "On"}
-                  </PostButton>
-                </div>
-              </td>
-              <td>
-                <div className="options">
-                  {user.role !== "admin" && (
+                    <p className="text">
+                      Status: <b>{user.shouldRedirectOnLimit ? "On" : "Off"}</b>
+                    </p>
                     <PostButton
-                      path="/api/delete_user"
+                      path="/api/change_redirect_config"
                       body={{
                         _id: user._id,
                       }}
-                      afterPost={afterDeleteUser}
-                      className="btn red"
+                      afterPost={afterChangeRedirectConfig}
+                      className="btn green"
                     >
-                      Delete
+                      Turn {user.shouldRedirectOnLimit ? "Off" : "On"}
                     </PostButton>
-                  )}
-                </div>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+                  </div>
+                </td>
+                <td>
+                  <div className="text-with-button">
+                    <p className="text">
+                      Status: <b>{user.shouldAlwaysRedirect ? "On" : "Off"}</b>
+                    </p>
+                    <PostButton
+                      path="/api/change_always_redirect_config"
+                      body={{
+                        _id: user._id,
+                      }}
+                      afterPost={afterChangeAlwaysRedirectConfig}
+                      className="btn green"
+                    >
+                      Turn {user.shouldAlwaysRedirect ? "Off" : "On"}
+                    </PostButton>
+                  </div>
+                </td>
+                <td>
+                  <div className="options">
+                    {user.role !== "admin" && (
+                      <PostButton
+                        path="/api/delete_user"
+                        body={{
+                          _id: user._id,
+                        }}
+                        afterPost={afterDeleteUser}
+                        className="btn red"
+                      >
+                        Delete
+                      </PostButton>
+                    )}
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
